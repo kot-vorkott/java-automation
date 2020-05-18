e0df23942acc201a7761ad6b5ba6e564f1ca3167
kot-vorkott <64366883+kot-vorkott@users.noreply.github.com>
2020-05-18 12:46:11 +0400

    2

---
+++ known-variables/src/variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args) {
+        int integer = 17448;
+        short shortInt = 3644;
+        byte theShortestInt =  - 126;
+        double floatingNumber = - 4745.4658574;
+        float shortFloat = 484.857f;
+        long longInt = - 68583435;
+        char a = 'ы';
+        boolean liar = false;
+        System.out.println("I'm integer: " + integer);
+        System.out.println("I'm short integer: " + shortInt);
+        System.out.println("I'm long integer: " + longInt);
+        System.out.println("I'm byte : " + theShortestInt);
+        System.out.println("I'm float: " + shortFloat);
+        System.out.println("I'm double: " + floatingNumber);
+        System.out.println("I'm char: " + a);
+        System.out.println("I'm boolean: " + liar);
+
+    }
+}
